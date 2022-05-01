a4c71a11d9b287658067b22e6a2a4c38d339e23d
Diaana B <99155537+DianaBarreraG@users.noreply.github.com>
2022-04-30 21:16:13 -0500

    Agregando ejemplo 1_2

---
+++ weekly_mission_2/example1/example1_2.js
@@ -0,0 +1,9 @@
+console.log("2. Creación de un objeto con propiedades")
+const myObject = {
+    name: "Diana",
+    age: 25,
+    mission: "Node JS"
+}
+
+console.log("Objeto con propiedades:")
+console.log(myObject)
