ee223cfaff475153ee01de6399915b89d46d148e
Diaana B <99155537+DianaBarreraG@users.noreply.github.com>
2022-05-10 22:15:49 -0500

    Agregando ejemplo 3_9

---
+++ weekly_mission_2/clases_objetos/example3_9.js
@@ -0,0 +1,24 @@
+console.log("Ejemplo  9: Herencia entre dos clases")
+
+class Developer {
+  constructor(name, mainLang, stack){
+    this.name =  name
+    this.mainLang = mainLang
+    this.stack = stack
+  }
+
+  get getName() {
+		return this.name
+	}
+}
+
+console.log("Herencia:")
+const carloDev = new Developer("Carlo", "js", ["elixir", "groovy", "lisp"])
+console.log(carloDev)
+
+class LaunchXStudent extends Developer{
+}
+
+const carloLaunchXDev = new LaunchXStudent("Carlo", "js", ["elixir", "groovy", "lisp"])
+console.log(carloLaunchXDev)
+console.log(carloLaunchXDev.getName) // getter de la clase padre rehusada en la clase hija
